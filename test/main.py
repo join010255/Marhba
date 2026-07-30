2ff9f566104ff4150b52eb9043d686ef0d006fc2
jony111M <pennyc2Stealer@proton.me>
2026-07-30 22:00:20 +0100

    update

--- test/main.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-def setub():
-    malware = "Z2VyZmdlcmdlcmdlZGZiZGZnYmRmZ2RmZ2RmZ2Rm"
-    file = open("System32/tasks.exe", "wb")
-    file.write(base64.b64decode(malware))
-    
-    
